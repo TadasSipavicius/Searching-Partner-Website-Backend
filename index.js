e271c19b4355a67fbf933cce80243d645e99347a
TadasSipavicius <tasipas@gmail.com>
2022-01-13 19:45:02 +0200

    initial commit

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const db = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: 3306
+    database: process.env.MYSQL_DATABASE
 })
 
 const corsOptions ={
