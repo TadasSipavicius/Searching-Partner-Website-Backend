0f5a62818d607d27f92a75181873fd7e923405d2
TadasSipavicius <tasipas@gmail.com>
2022-01-13 19:29:31 +0200

    trying to fix heroku server connection to mysql

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const db = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: 3306
 })
 
 const corsOptions ={
